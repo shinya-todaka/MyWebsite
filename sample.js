7e33dbf2b0e2aa3803ec6744b400de6e6d246b02
shinya-todaka <tdk0105.strawberry@gmail.com>
2023-04-01 15:56:02 +0900

    nativeから値を受け取るパターン追加

--- sample.js
+++ sample.js
@@ -1,10 +1,9 @@
-function onClickButton() {
-    console.log("on click!!!!");
-}
-
 var count = 0;
-
 function passCountMessageToNative() {
     count += 1;
     window.webkit.messageHandlers.count.postMessage(count);
+}
+
+window.countFromNative = function(number) {
+    alert("got count from native ${number}");
 }
