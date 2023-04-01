824ac6e395c51ad43b7b6f3c4954898d438ff22f
shinya-todaka <tdk0105.strawberry@gmail.com>
2023-04-01 15:15:56 +0900

    ネイティブに値を渡すときのコードを追加

--- sample.js
+++ sample.js
@@ -1,3 +1,10 @@
 function onClickButton() {
     console.log("on click!!!!");
 }
+
+var count = 0;
+
+function passCountMessageToNative() {
+    count += 1;
+    window.webkit.messageHandlers.count.postMessage(count);
+}
