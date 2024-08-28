ac782dffdd55188da91cd536faad87815520b6d6
shinya-todaka <tdk0105.strawberry@gmail.com>
2024-08-28 19:47:48 +0900

    add hide or show button

--- sample.js
+++ sample.js
@@ -7,3 +7,17 @@ function passCountMessageToNative() {
 function increment(number) {
     return number + 1
 }
+
+var showPasswordButton = document.getElementById("toggleShowPasswordButton");
+showPasswordButton.addEventListener("click", togglePasswordVisibility);
+
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("passwordInput");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    showPasswordButton.textContent = "非表示";
+  } else {
+    passwordInput.type = "password";
+    showPasswordButton.textContent = "表示";
+  }
+}
